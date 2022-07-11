0295add8d53e4a98d4c0d56e6bb7e065cf2d8ebf
crolka <christinemrolka@gmail.com>
2022-07-11 14:45:44 -0400

    using sideEffects
    Understanding the basic concepts; especially about the timing of when the effects run.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,32 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // !!! This will run ONLY when the dependencies change!!!
+  //      Arguments: 1. () => {} - function that runs when the dependencies change
+  //                 2. []       - an array of dependencies.
+  useEffect(() => {
+    const storedUserLogInInfo = localStorage.getItem('isLoggedIn');
+
+    if (storedUserLogInInfo === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+
+    // localStorage is provided by the browser. the first argument
+    //  is the key identifier, and the second the value.
+    // the key & value can be anything of your choosing.
+    // This handler only runs when the button on the login screen is pressed.
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    // removes the item from local storage so we don't have to do it manually.
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
