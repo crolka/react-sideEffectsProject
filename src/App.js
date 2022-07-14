b6a672cf6c049194ce8441e5e8a16e3e936685aa
crolka <christinemrolka@gmail.com>
2022-07-14 12:11:59 -0400

    Implemented Context using Provider and Consumer
    Learned about using Context to avoid prop chain dependancies.
    
    Setup context registration, providers, and listeners. The listener uses a Consumer.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import AuthContext from "./store/auth-context";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -10,17 +11,17 @@ function App() {
   // !!! This will run ONLY when the dependencies change!!!
   //      Arguments: 1. () => {
   //                      ...
-  //                      return () => {...} - this is the cleanup funciton 
-  //                                            that runs before the state 
+  //                      return () => {...} - this is the cleanup funciton
+  //                                            that runs before the state
   //                                            change functions run.
   //                    } - function that runs when the dependencies change
   //                 2. []       - an array of dependencies.
   // in this scenario, we are looking at maintaining a persistent
   // state when the page reloads.
   useEffect(() => {
-    const storedUserLogInInfo = localStorage.getItem('isLoggedIn');
+    const storedUserLogInInfo = localStorage.getItem("isLoggedIn");
 
-    if (storedUserLogInInfo === '1') {
+    if (storedUserLogInInfo === "1") {
       setIsLoggedIn(true);
     }
   }, []);
@@ -33,24 +34,26 @@ function App() {
     //  is the key identifier, and the second the value.
     // the key & value can be anything of your choosing.
     // This handler only runs when the button on the login screen is pressed.
-    localStorage.setItem('isLoggedIn', '1');
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
     // removes the item from local storage so we don't have to do it manually.
-    localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider value={ 
+      {isLoggedIn: isLoggedIn} 
+      }>
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
+// this function is defined outside the component function because 
+// it does not need to interact with any data within the component.
+// any data that is required by this function will be pass as an 
+// argument automatically by React.
+const emailReducer = (state, action) => {
+  if (action.type === 'USER_INPUT') {
+    return {value: action.val, isValid: action.val.includes('@') };
+  }
+  if (action.type === 'INPUT_BLUR') {
+    return {value: state.value, isValid: state.value.includes('@') };
+  }
+
+  return {value: '', isValid: false };
+};
+
+const passwordReducer = (state, action) => {
+  if (action.type === 'USER_INPUT') {
+    return {value: action.val, isValid: action.val.trim().length > 6};
+  }
+
+  if (action.type === 'INPUT_BLUR') {
+    return {value: state.value, isValid: state.value.trim().length > 6};
+  }
+
+  return {value: '', isValid: false };
+};
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
-  const [passwordIsValid, setPasswordIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState('');
+  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
+
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {value: '', isValid: null });
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {value: '', isValid: null});
+
+  // This is object destructuring. Works the same as array destructuring but only with objects. In this
+  // example, I am defining an alias because the key names are the same for each of the emailState and 
+  // passwordState objects.
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } = passwordState;
 
   useEffect(() => {
     console.log('EFFECT RUNNING');
@@ -19,47 +55,61 @@ const Login = (props) => {
     };
   }, []);
 
-  // useEffect(() => {
-  //   const identifierTimeout = setTimeout( () => {
-  //     // console.log('Checking the form\'s validity');
-  //     setFormIsValid(
-  //       enteredPassword.trim().length > 6 && enteredEmail.includes('@')
-  //     );
-  //   }, 500);
+  // using useEffect will guarantee that formValidity is based on the latest 
+  // state snapshot for our email and password values.
+  useEffect(() => {
+    const identifierTimeout = setTimeout( () => {
+      console.log('Checking the form\'s validity');
+      setFormIsValid(
+        passwordIsValid && emailIsValid
+      );
+    }, 500);
     
-  //   return () => {
-  //     // console.log('CLEANUP!');
-  //     clearTimeout(identifierTimeout); 
-  //   }
-  // }, [enteredEmail, enteredPassword]);
+    return () => {
+      console.log('CLEANUP!');
+      clearTimeout(identifierTimeout); 
+    }
+  }, [emailIsValid, passwordIsValid]);
+  //}, [passwordState, emailState]);
   
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    //setEnteredEmail(event.target.value);
+    dispatchEmail({type: 'USER_INPUT', val: event.target.value});
 
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
+    // event.target.value.includes('@') && passwordState.isValid
+
+    // having the form validity dependent on two separate values 
+    // means that we could be basing the validity of the form
+    // on an old email or password validity value. Instead we can 
+    // reuse the useEffect method.
+    // setFormIsValid(
+    //   emailState.isValid && passwordState.isValid
+    // );
   };
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
-    
-    setFormIsValid(
-      enteredEmail.includes('@') && event.target.value.trim().length > 6
-    );
+    // setEnteredPassword(event.target.value);
+    dispatchPassword({type: 'USER_INPUT', val: event.target.value});
+
+    // emailState.isValid && event.target.value.trim().length > 6
+    // setFormIsValid(
+    //   emailState.isValid && passwordState.isValid
+    // );
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    // setEmailIsValid(emailState.isValid);
+    dispatchEmail({type: 'INPUT_BLUR'});
   };
 
   const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
+    // setPasswordIsValid(enteredPassword.trim().length > 6);
+    dispatchPassword({type: 'INPUT_BLUR'});
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, passwordState.value);
   };
 
   return (
@@ -67,28 +117,28 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
+            passwordState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
-            value={enteredPassword}
+            value={passwordState.value}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
